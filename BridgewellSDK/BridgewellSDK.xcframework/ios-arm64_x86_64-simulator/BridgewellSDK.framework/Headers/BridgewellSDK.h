d5bc0558aa66f59b7d26a4e50d92ba118030d109
Mai Quân Nguyễn <quannm1@vmogroup.com>
2024-04-26 09:38:17 +0700

    - add support for x86-64 for iOS simulator - add support for cocoapods with version from tag

---
+++ BridgewellSDK/BridgewellSDK.xcframework/ios-arm64_x86_64-simulator/BridgewellSDK.framework/Headers/BridgewellSDK.h
@@ -0,0 +1,18 @@
+//
+//  BridgewellSDK.h
+//  BridgewellSDK
+//
+//  Created by Nalou Nguyen on 1/4/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BridgewellSDK.
+FOUNDATION_EXPORT double BridgewellSDKVersionNumber;
+
+//! Project version string for BridgewellSDK.
+FOUNDATION_EXPORT const unsigned char BridgewellSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BridgewellSDK/PublicHeader.h>
+
+
