2e767a2d53362de3769597a541441a60658c04b7
charly.chiu <charly.chiu@bridgewell.com>
2024-08-30 12:11:28 +0800

    release: Bump to 2.0.0

---
+++ BridgewellSDK/BridgewellSDK.xcframework/ios-arm64_x86_64-simulator/BridgewellSDK.framework/Headers/WrappingObjC.h
@@ -0,0 +1,63 @@
+//
+//  WrappingObjC.h
+//  BridgewellSDK
+//
+//  Created by Nguyễn Mai Quân on 28/8/24.
+//
+
+#ifndef WrappingObjC_h
+#define WrappingObjC_h
+#import <Foundation/Foundation.h>
+#import <PrebidMobile/PrebidMobile.h>
+
+typedef id<InterstitialAdUnitDelegate> BwsInterstitialAdUnitDelegate;
+typedef InterstitialRenderingAdUnit* BwsInterstitialRenderingAdUnit;
+typedef NativeRequest* BwsNativeRequest;
+typedef NativeAd* BwsNativeAd;
+typedef NativeAsset* BwsNativeAsset;
+typedef NativeAssetImage* BwsNativeAssetImage;
+typedef ImageAsset* BwsImageAsset;
+typedef NativeAssetTitle* BwsNativeAssetTitle;
+typedef NativeAssetData* BwsNativeAssetData;
+typedef NativeEventTracker* BwsNativeEventTracker;
+typedef ContextType* BwsContextType;
+typedef PlacementType* BwsPlacementType;
+typedef ContextSubType* BwsContextSubType;
+typedef RewardedVideoAdUnit* BwsRewardedVideoAdUnit;
+typedef InstreamVideoAdUnit* BwsInstreamVideoAdUnit;
+typedef IMAUtils* BwsIMAUtils;
+typedef PrebidHost* BwsBridgewellHost;
+
+typedef enum DataAsset BwsDataAsset;
+typedef EventType* BwsEventType;
+typedef EventTracking* BwsEventTracking;
+
+typedef id<RewardedAdUnitDelegate> BwsRewardedAdUnitDelegate;
+typedef RewardedAdUnit* BwsRewardedAdUnit;
+typedef MediationBannerAdUnit* BwsMediationBannerAdUnit;
+typedef MediationInterstitialAdUnit* BwsMediationInterstitialAdUnit;
+typedef MediationNativeAdUnit* BwsMediationNativeAdUnit;
+typedef AdViewUtils* BwsAdViewUtils;
+typedef BannerAdUnit* BwsBannerAdUnit;
+typedef BannerParameters* BwsBannerParameters;
+typedef Signals* BwsSignals;
+typedef InterstitialAdUnit* BwsInterstitialAdUnit;
+typedef VideoParameters* BwsVideoParameters;
+typedef id<NativeAdDelegate> BwsNativeAdDelegate;
+typedef AdUnit* BwsBridgewellAdUnit;
+typedef NativeParameters* BwsNativeParameters;
+typedef PrebidRequest* BwsRequest;
+typedef Utils* BwsUtils;
+typedef MediationRewardedAdUnit* BwsMediationRewardedAdUnit;
+typedef PBMLogLevel* BwsLogLevel;
+typedef AdFormat* BwsAdFormat;
+typedef PBMAdPosition* BwsAdPosition;
+
+typedef void (^BwsRegisterWebviewCallback)(NSString * _Nullable, NSError * _Nullable);
+
+#endif /* WrappingObjC_h */
+
+
+
+
+
